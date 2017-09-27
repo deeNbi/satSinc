397c818e016f163c33de82d8ec223b557590c7c9
jeonjimin <jeonjimin8050@gmail.com>
2017-09-27 10:18:55 +0900

    20170927

--- FinalProject_SAT/src/main/java/com/sinc/sat/analy/model/vo/AnalysisVO.java
+++ FinalProject_SAT/src/main/java/com/sinc/sat/analy/model/vo/AnalysisVO.java
@@ -1,5 +1,14 @@
 package com.sinc.sat.analy.model.vo;
 
+import java.util.Date;
+
 public class AnalysisVO {
 
+	private int analy_no;
+	private Date analy_date;
+	private String analy_id;
+	private int tm_mono;
+	private int dl_mono;
+	
+	
 }
